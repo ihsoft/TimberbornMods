1abfdb43bdb25ce38138f6da01369f39ea53361d
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-12 15:49:11 -0700

    Improve prefab patcher

--- SmartPower/Configurator.cs
+++ SmartPower/Configurator.cs
@@ -25,10 +25,12 @@ sealed class Configurator : IConfigurator {
 
 [HarmonyPatch(typeof(Instantiator), nameof(Instantiator.InstantiateInactive))]
 public static class PrefabInstantiatorPatch {
+  static readonly PrefabPatcher.RequiredComponentsDep SmartAttractionDeps =
+      new(typeof(Enterable), typeof(Attraction), typeof(MechanicalBuilding));
+
   static bool Prefix(GameObject prefab) {
-    PrefabPatcher.ReplaceComponent<GoodPoweredGenerator, SmartGoodPoweredGenerator>(prefab);
-    PrefabPatcher.AddComponent<SmartPoweredAttraction>(
-        prefab, new[]{ typeof(Enterable), typeof(Attraction), typeof(MechanicalBuilding) });
+    PrefabPatcher.ReplaceComponent<GoodPoweredGenerator, SmartGoodPoweredGenerator>(prefab, _ => true);
+    PrefabPatcher.AddComponent<SmartPoweredAttraction>(prefab, SmartAttractionDeps.Check);
     return true;
   }
 }
--- SmartPower/PrefabPatcher.cs
+++ SmartPower/PrefabPatcher.cs
@@ -19,14 +19,10 @@ namespace SmartPower {
 public static class PrefabPatcher {
   /// <summary>Adds a new component to prefab based in the filter condition.</summary>
   /// <param name="prefab">Prefab to add the component to.</param>
-  /// <param name="requiredComponents">A set of components that must exist on the prefab.</param>
+  /// <param name="checkDeps">Filter condition.</param>
   /// <typeparam name="T">type of the component to add.</typeparam>
-  public static void AddComponent<T>(GameObject prefab, Type[] requiredComponents) where T : BaseComponent {
-    if (prefab.GetComponent<T>() != null) {
-      return;
-    }
-    var components = prefab.GetComponents<BaseComponent>().Select(x => x.GetType()).ToArray();
-    if (!requiredComponents.All(components.Contains)) {
+  public static void AddComponent<T>(GameObject prefab, Func<GameObject, bool> checkDeps) where T : BaseComponent {
+    if (prefab.GetComponent<T>() != null || !checkDeps(prefab)) {
       return;
     }
     DebugEx.Fine("Add component to prefab: name={0}, component={1}", prefab.name, typeof(T));
@@ -38,17 +34,30 @@ public static class PrefabPatcher {
   /// Use it with care due to other components on the prefab may be expecting the old component to exist.
   /// </remarks>
   /// <param name="prefab">Prefab to replace the component on.</param>
+  /// <param name="checkDeps">Filter condition.</param>
   /// <typeparam name="TSource">type of the component to remove.</typeparam>
   /// <typeparam name="TTarget">type of the component to add.</typeparam>
-  public static void ReplaceComponent<TSource, TTarget>(GameObject prefab)
+  public static void ReplaceComponent<TSource, TTarget>(GameObject prefab, Func<GameObject, bool> checkDeps)
       where TSource : BaseComponent where TTarget : BaseComponent {
-    if (prefab.GetComponent<TTarget>() != null || prefab.GetComponent<TSource>() == null) {
+    if (prefab.GetComponent<TTarget>() != null || prefab.GetComponent<TSource>() == null || !checkDeps(prefab)) {
       return;
     }
     DebugEx.Fine("Replace component on prefab: name={0}, oldComponent={1}, newComponent={2}",
                  prefab.name, typeof(TSource), typeof(TTarget));
     Object.DestroyImmediate(prefab.GetComponent<TSource>());
     prefab.AddComponent<TTarget>();
+  }
+
+  /// <summary>Helper filter class to check the existing and required components on prefab.</summary>
+  public class RequiredComponentsDep {
+    readonly Type[] _requiredComponents;
+    public RequiredComponentsDep(params Type[] requiredComponents) {
+      _requiredComponents = requiredComponents;
+    }
+    public bool Check(GameObject prefab) {
+      var components = prefab.GetComponents<BaseComponent>().Select(x => x.GetType()).ToArray();
+      return _requiredComponents.All(components.Contains);
+    }
   }
 }
 
