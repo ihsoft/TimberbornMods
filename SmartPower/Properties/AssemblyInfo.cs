75fd50aa7fd735240ec0c88e66d19f5b90c98bda
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-12 23:03:03 -0700

    Add property file

---
+++ SmartPower/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("IHSoft")]
+[assembly: AssemblyProduct("Smart Power: Plugin for Timberborn game")]
+[assembly: AssemblyCopyright("Public domain")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B13C37FF-EF29-4C28-A039-6E96D13DDE7D")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
