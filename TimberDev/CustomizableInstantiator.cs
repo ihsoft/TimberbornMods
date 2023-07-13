ce0090672ab8ae63e7532dc74f1b208d7afe03ed
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-12 21:54:48 -0700

    Create CustomizableInstantiator

---
+++ TimberDev/CustomizableInstantiator.cs
@@ -0,0 +1,55 @@
+// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using System.Collections.Generic;
+using Bindito.Unity;
+using HarmonyLib;
+using TimberDev.Utils;
+using UnityEngine;
+
+// ReSharper disable UnusedMember.Local
+// ReSharper disable once CheckNamespace
+namespace IgorZ.TimberDev.CustomInstantiator {
+
+/// <summary>Class that allows customizing components on prefabs.</summary>
+/// <remarks>
+/// For every prefab that is used in the game a custom code will be called before creating any instances. It allows
+/// customizing the prefab. E.g. a new component can be added, an existing component replaced, or the prefab components
+/// can be configured differently from their normal asset state.
+/// </remarks>
+/// <seealso cref="PrefabPatcher"/>
+public static class CustomizableInstantiator {
+  /// <summary>Adds a patcher method. Must be called from the <c>InGame</c> configurator.</summary>
+  /// <remarks>
+  /// The patcher method will be called exactly once for every unique prefab. The prefab patchers are re-applied on
+  /// every game load.
+  /// </remarks>
+  /// <param name="patchId">
+  /// Unique id of the patch. Adding multiple patchers with teh same ID will result in overwriting.
+  /// </param>
+  /// <param name="patchFn">The method to call on the prefab being patched.</param>
+  public static void AddPatcher(string patchId, Action<GameObject> patchFn) {
+    HarmonyPatcher.PatchRepeated(typeof(CustomizableInstantiator).FullName, typeof(PrefabInstantiatePatch));
+    PrefabInstantiatePatch.Patchers[patchId] = patchFn;
+    PrefabInstantiatePatch.PatchedCache.Clear();
+  }
+
+  [HarmonyPatch(typeof(Instantiator), nameof(Instantiator.InstantiateInactive))]
+  static class PrefabInstantiatePatch {
+    public static readonly HashSet<string> PatchedCache = new();
+    public static readonly Dictionary<string, Action<GameObject>> Patchers = new();
+
+    static void Prefix(GameObject prefab) {
+      if (!PatchedCache.Add(prefab.name)) {
+        return;
+      }
+      foreach (var patcher in Patchers.Values) {
+        patcher(prefab);
+      }
+    }
+  }
+}
+
+}
--- TimberDev/HarmonyPatcher.cs
+++ TimberDev/HarmonyPatcher.cs
@@ -9,11 +9,31 @@ using UnityDev.Utils.LogUtilsLite;
 namespace TimberDev.Utils {
 
 public static class HarmonyPatcher {
+  /// <summary>Applies Harmony patches.</summary>
+  /// <remarks>This method ensures that no Harmony patches are applied twice.</remarks>
+  /// <param name="patchId">Unique patch ID. Duplicated patches for the same ID will be silently ignored.</param>
+  /// <param name="patchTypes">Static types that define Harmony patches.</param>
+  /// <exception cref="InvalidOperationException">if a patch with <paramref name="patchId"/> was already applied.</exception>
   public static void PatchWithNoDuplicates(string patchId, params Type[] patchTypes) {
     if (Harmony.HasAnyPatches(patchId)) {
-      DebugEx.Warning("Skip duplicated patches: {0}", patchId);
-      return;
+      throw new InvalidOperationException("Patch already applied: " + patchId);
     }
+    Patch(patchId, patchTypes);
+  }
+
+  /// <summary>Applies Harmony patches.</summary>
+  /// <remarks>
+  /// This method can be called multiple time for the same patch ID. The duplicates will just be ignored.
+  /// </remarks>
+  /// <param name="patchId">Unique patch ID. Duplicated patches for the same ID will be silently ignored.</param>
+  /// <param name="patchTypes">Static types that define Harmony patches.</param>
+  public static void PatchRepeated(string patchId, params Type[] patchTypes) {
+    if (!Harmony.HasAnyPatches(patchId)) {
+      Patch(patchId, patchTypes);
+    }
+  }
+
+  static void Patch(string patchId, Type[] patchTypes) {
     var harmony = new Harmony(patchId);
     foreach (var type in patchTypes) {
       harmony.PatchAll(type);
