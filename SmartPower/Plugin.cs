9392b6068455c052250910f34c15f84343cf0f90
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-12 21:57:37 -0700

    Use custom instantiator

--- SmartPower/Plugin.cs
+++ SmartPower/Plugin.cs
@@ -3,57 +3,54 @@
 // License: Public Domain
 
 using System.Reflection;
-using Bindito.Unity;
+using Bindito.Core;
 using HarmonyLib;
-using TimberApi.ConsoleSystem;
-using TimberApi.ModSystem;
+using IgorZ.TimberDev.CustomInstantiator;
+using TimberApi.ConfiguratorSystem;
+using TimberApi.SceneSystem;
 using Timberborn.Attractions;
 using Timberborn.EnterableSystem;
 using Timberborn.Localization;
 using Timberborn.MechanicalSystem;
 using Timberborn.PowerGenerating;
 using TimberDev.Utils;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 // ReSharper disable InconsistentNaming
 // ReSharper disable UnusedMember.Local
 namespace SmartPower {
 
+[Configurator(SceneEntrypoint.InGame)]
 // ReSharper disable once UnusedType.Global
-sealed class Plugin : IModEntrypoint {
-  public void Entry(IMod mod, IConsoleWriter consoleWriter) {
-    HarmonyPatcher.PatchWithNoDuplicates(
-        "IgorZ.SmartPower", typeof(PrefabInstantiatorPatch), typeof(NetworkFragmentServicePatch));
-  }
-}
-
-[HarmonyPatch(typeof(Instantiator), nameof(Instantiator.InstantiateInactive))]
-public static class PrefabInstantiatorPatch {
+sealed class Configurator : IConfigurator {
   static readonly PrefabPatcher.RequiredComponentsDep SmartAttractionDeps =
       new(typeof(Enterable), typeof(Attraction), typeof(MechanicalBuilding));
 
-  static bool Prefix(GameObject prefab) {
-    PrefabPatcher.ReplaceComponent<GoodPoweredGenerator, SmartGoodPoweredGenerator>(prefab, _ => true);
-    PrefabPatcher.AddComponent<SmartPoweredAttraction>(prefab, SmartAttractionDeps.Check);
-    return true;
-  }
-}
-
-[HarmonyPatch]
-public static class NetworkFragmentServicePatch {
-  const string NetworkFragmentServiceClassName = "Timberborn.MechanicalSystemUI.NetworkFragmentService";
-  const string MethodName = "Update";
-
-  static MethodBase TargetMethod() {
-    var type = AccessTools.TypeByName(NetworkFragmentServiceClassName);
-    return AccessTools.FirstMethod(type, method => method.Name == MethodName);
+  public void Configure(IContainerDefinition containerDefinition) {
+    HarmonyPatcher.PatchRepeated(typeof(Configurator).FullName, typeof(NetworkFragmentServicePatch));
+    CustomizableInstantiator.AddPatcher(
+        typeof(Configurator).FullName,
+        prefab => {
+          PrefabPatcher.ReplaceComponent<GoodPoweredGenerator, SmartGoodPoweredGenerator>(prefab, _ => true);
+          PrefabPatcher.AddComponent<SmartPoweredAttraction>(prefab, SmartAttractionDeps.Check);
+        });
   }
 
-  static void Postfix(MechanicalNode mechanicalNode, Label ____label, ILoc ____loc) {
-    var text = BatteryStateTextFormatter.FormatBatteryText(mechanicalNode, ____loc);
-    if (text != "") {
-      ____label.text += "\n" + text;
+  [HarmonyPatch]
+  static class NetworkFragmentServicePatch {
+    const string NetworkFragmentServiceClassName = "Timberborn.MechanicalSystemUI.NetworkFragmentService";
+    const string MethodName = "Update";
+
+    static MethodBase TargetMethod() {
+      var type = AccessTools.TypeByName(NetworkFragmentServiceClassName);
+      return AccessTools.FirstMethod(type, method => method.Name == MethodName);
+    }
+
+    static void Postfix(MechanicalNode mechanicalNode, Label ____label, ILoc ____loc) {
+      var text = BatteryStateTextFormatter.FormatBatteryText(mechanicalNode, ____loc);
+      if (text != "") {
+        ____label.text += "\n" + text;
+      }
     }
   }
 }
