f519728aa203a415c8a2d664719c81ed5c451063
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-22 21:45:00 -0700

    Create ThreadedLogsRecorder.cs

---
+++ WaterValveTest/TimberDev/ThreadedLogsRecorder.cs
@@ -0,0 +1,46 @@
+// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System.Collections.Concurrent;
+using System.Threading;
+using Timberborn.SingletonSystem;
+using Timberborn.TickSystem;
+using UnityDev.Utils.LogUtilsLite;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace IgorZ.TimberDev.Logging {
+
+/// <summary>Helper to catch logs from the non-main threads and spitting them out to the main logger.</summary>
+/// <remarks>Bind this class via Bindito as singleton to make it working.</remarks>
+class ThreadedLogsRecorder : ILoadableSingleton, ITickableSingleton {
+  ConcurrentQueue<string> _logRecords = new();
+  Thread _mainUnityThread;
+
+  public void Load() {
+    _mainUnityThread = Thread.CurrentThread;
+    Application.logMessageReceivedThreaded -= HandleThreadLog;
+    Application.logMessageReceivedThreaded += HandleThreadLog;
+  }
+
+  public void Tick() {
+    if (_logRecords.IsEmpty) {
+      return;
+    }
+    var logs = _logRecords.ToArray();
+    _logRecords = new ConcurrentQueue<string>();
+    foreach (var t in logs) {
+      DebugEx.Warning(t);
+    }
+  }
+
+  void HandleThreadLog(string logString, string stackTrace, LogType type) {
+    if (Thread.CurrentThread == _mainUnityThread) {
+      return;
+    }
+    _logRecords.Enqueue($"[{type}] [Thread#{Thread.CurrentThread.ManagedThreadId}] {logString}");
+  }
+}
+
+}
