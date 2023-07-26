2885e7ded7203858820a96371ddf921974025cae
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-07-26 02:25:29 -0700

    Stop using links to common files

--- UnityDevLite/TypeUtils.cs
+++ UnityDevLite/TypeUtils.cs
@@ -9,7 +9,7 @@ using System.Linq;
 namespace UnityDev.Utils.Reflections {
 
 /// <summary>Various utils to deal with the types via reflections.</summary>
-public static class TypeUtils {
+static class TypeUtils {
   /// <summary>Gets the type by it's full name. Even if the type is internal.</summary>
   /// <exception cref="ArgumentException">if the type cannot be found in the current appdomain.</exception>
   public static Type GetInternalType(string fullTypeName) {
