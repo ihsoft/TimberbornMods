056187d096abc728211428a36577d5d51da0c859
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2023-08-05 00:23:51 -0700

    Drop unused code

--- Automation/Core/AutomationService.cs
+++ Automation/Core/AutomationService.cs
@@ -24,7 +24,6 @@ public sealed class AutomationService : IPostLoadableSingleton {
   readonly Color _highlightColor = Color.cyan * 0.5f;
   readonly Highlighter _highlighter;
   bool _highlightingEnabled;
-  BaseComponent _rootComponent;
   #endregion
 
   public AutomationService(EventBus eventBus, Highlighter highlighter, BaseInstantiator baseInstantiator, ILoc loc) {
@@ -66,24 +65,6 @@ public sealed class AutomationService : IPostLoadableSingleton {
     _highlightingEnabled = false;
     _highlighter.UnhighlightAllSecondary();
   }
-
-  /// <summary>Obtains a global automation behavior object.</summary>
-  /// <remarks>
-  /// Such object exists as singletons in the game scene. It's an object that monitors the overall game state and
-  /// doesn't depend on a specific game object.
-  /// </remarks>
-  /// <typeparam name="T">the behavior type to return or create</typeparam>
-  /// <returns>Existing or new instance of the behavior of the requested type.</returns>
-  public T GetGlobalBehavior<T>() where T : AutomationConditionBehaviorBase {
-    if (_rootComponent == null) {
-      var prefabObj = new GameObject("#rootPrefab-" + GetType().ToString());
-      var rootObj = BaseInstantiator.InstantiateInactive(prefabObj, null);
-      Object.Destroy(prefabObj);
-      _rootComponent = BaseInstantiator.AddComponent<RootComponent>(rootObj);
-    }
-    return _rootComponent.GetComponentFast<T>() ?? BaseInstantiator.AddComponent<T>(_rootComponent.GameObjectFast);
-  }
-  internal sealed class RootComponent : BaseComponent {}
   #endregion
 
   #region Implementation
