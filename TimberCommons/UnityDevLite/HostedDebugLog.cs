d45701d314fa933dbd8c9fc16390c7bfdc9e4a5a
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-01-13 23:44:38 -0800

    Add UnityDev utils.

---
+++ TimberCommons/UnityDevLite/HostedDebugLog.cs
@@ -0,0 +1,52 @@
+﻿// Unity Development tools.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using System.Diagnostics.CodeAnalysis;
+using Timberborn.BaseComponentSystem;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace UnityDev.Utils.LogUtilsLite {
+
+/// <summary>A light version of logging utils from UnityDev specialized for Timberborn.</summary>
+/// <remarks>https://github.com/ihsoft/UnityDev_Utils</remarks>
+/// <seealso cref="DebugEx.LoggingSettings"/>
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+static class HostedDebugLog {
+  /// <summary>Logs a formatted INFO message with a host identifier.</summary>
+  public static void Info(BaseComponent host, string format, params object[] args) {
+    Log(LogType.Log, host, format, args);
+  }
+
+  /// <summary>
+  /// Logs a formatted INFO message with a host identifier when the <i>verbose</i> logging mode is enabled.
+  /// </summary>
+  public static void Fine(BaseComponent host, string format, params object[] args) {
+    if (DebugEx.LoggingSettings.VerbosityLevel > 0) {
+      Log(LogType.Log, host, format, args);
+    }
+  }
+
+  /// <summary>Logs a formatted WARNING message with a host identifier.</summary>
+  public static void Warning(BaseComponent host, string format, params object[] args) {
+    Log(LogType.Warning, host, format, args);
+  }
+
+  /// <summary>Logs a formatted ERROR message with a host identifier.</summary>
+  public static void Error(BaseComponent host, string format, params object[] args) {
+    Log(LogType.Error, host, format, args);
+  }
+
+  /// <summary>Generic method to emit a hosted log record.</summary>
+  /// <param name="type">The type of the log record.</param>
+  /// <param name="host">The host object which is bound to the log record. It can be <c>null</c>.</param>
+  /// <param name="format">The format string for the log message.</param>
+  /// <param name="args">The arguments for the format string.</param>
+  /// <seealso cref="DebugEx.ObjectToString"/>
+  public static void Log(LogType type, object host, string format, params object[] args) {
+    DebugEx.Log(type, DebugEx.ObjectToString(host) + " " + format, args);
+  }
+}
+
+}  // namespace
---
+++ TimberCommons/UnityDevLite/ReflectedField.cs
@@ -0,0 +1,139 @@
+﻿// UnityDev Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityDev.Utils.LogUtilsLite;
+
+// ReSharper disable once CheckNamespace
+namespace UnityDev.Utils.Reflections {
+
+/// <summary>Wrapper to implement efficient access to the class fields via reflection.</summary>
+/// <remarks>It ignores access scope.</remarks>
+/// <typeparam name="T">type of the class to get the field for.</typeparam>
+/// <typeparam name="TV">type of the field value.</typeparam>
+sealed class ReflectedField<T, TV> {
+  readonly FieldInfo _fieldInfo;
+
+  /// <summary>Creates the reflection for the field.</summary>
+  /// <param name="fieldName">The name of the field.</param>
+  /// <param name="throwOnFailure">
+  /// If <c>true</c> then the code will throw in case of the field cannot be obtained. Otherwise, the wrapper will only
+  /// log the error and will ignoring all set/get operations.
+  /// </param>
+  /// <seealso cref="IsValid"/>
+  public ReflectedField(string fieldName, bool throwOnFailure = false) {
+    _fieldInfo = typeof(T).GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+    if (_fieldInfo == null) {
+      if (throwOnFailure) {
+        throw new InvalidOperationException($"Cannot obtain field {typeof(T)}.{fieldName}");
+      }
+      DebugEx.Error("Cannot obtain field {0} from {1}", fieldName, typeof(T));
+    } else {
+      var fieldType = _fieldInfo.FieldType;
+      if (typeof(TV) != fieldType) {
+        if (throwOnFailure) {
+          throw new InvalidOperationException($"Expected field type {typeof(TV)}, but found {fieldType}");
+        }
+        DebugEx.Error("Incompatible field types: requested={0}, actual={1}", typeof(TV), fieldType);
+        _fieldInfo = null;
+      }
+    }
+  }
+
+  /// <summary>Indicates if the target field was found and ready to use.</summary>
+  public bool IsValid() {
+    return _fieldInfo != null;
+  }
+
+  /// <summary>Gets the field value or returns a default value if the field is not found.</summary>
+  public TV Get(T instance) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : default(TV);
+  }
+
+  /// <summary>Gets the field value or returns the provided default value if the field is not found.</summary>
+  public TV Get(T instance, TV defaultValue) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : defaultValue;
+  }
+
+  /// <summary>Sets the field value or does nothing if the field is not found.</summary>
+  public void Set(T instance, TV value) {
+    if (_fieldInfo != null) {
+      _fieldInfo.SetValue(instance, value);
+    }
+  }
+}
+
+
+/// <summary>Wrapper to implement efficient access to the class fields via reflection.</summary>
+/// <remarks>It ignores access scope.</remarks>
+/// <typeparam name="TV">type of the field value.</typeparam>
+sealed class ReflectedField<TV> {
+  readonly FieldInfo _fieldInfo;
+
+  /// <summary>Creates the reflection for the field.</summary>
+  /// <param name="type">Type of the class to get the field for.</param>
+  /// <param name="fieldName">The name of the field.</param>
+  /// <param name="throwOnFailure">
+  /// If <c>true</c> then the code will throw in case of the field cannot be obtained. Otherwise, the wrapper will only
+  /// log the error and will ignoring all set/get operations.
+  /// </param>
+  /// <seealso cref="IsValid"/>
+  public ReflectedField(IReflect type, string fieldName, bool throwOnFailure = false) {
+    //MakeFromType(type, fieldName, throwOnFailure);
+    _fieldInfo = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+    if (_fieldInfo == null) {
+      if (throwOnFailure) {
+        throw new InvalidOperationException($"Cannot obtain field {type}.{fieldName}");
+      }
+      DebugEx.Error("Cannot obtain field {0} from {1}", fieldName, type);
+    } else {
+      var fieldType = _fieldInfo.FieldType;
+      if (typeof(TV) != fieldType) {
+        if (throwOnFailure) {
+          throw new InvalidOperationException($"Expected field type {typeof(TV)}, but found {fieldType}");
+        }
+        DebugEx.Error("Incompatible field types: requested={0}, actual={1}", typeof(TV), fieldType);
+        _fieldInfo = null;
+      }
+    }
+  }
+
+  /// <summary>Creates the reflection for the field.</summary>
+  /// <param name="fullTypeName">Full type name. It can be internal type.</param>
+  /// <param name="fieldName">The name of the field.</param>
+  /// <param name="throwOnFailure">
+  /// If <c>true</c> then the code will throw in case of the field cannot be obtained. Otherwise, the wrapper will only
+  /// log the error and will ignoring all set/get operations.
+  /// </param>
+  /// <seealso cref="IsValid"/>
+  public ReflectedField(string fullTypeName, string fieldName, bool throwOnFailure = false)
+    : this(TypeUtils.GetInternalType(fullTypeName), fieldName, throwOnFailure) {
+  }
+
+  /// <summary>Indicates if the target field was found and ready to use.</summary>
+  public bool IsValid() {
+    return _fieldInfo != null;
+  }
+
+  /// <summary>Gets the field value or returns a default value if the field is not found.</summary>
+  public TV Get(object instance) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : default;
+  }
+
+  /// <summary>Gets the field value or returns the provided default value if the field is not found.</summary>
+  public TV Get(object instance, TV defaultValue) {
+    return _fieldInfo != null ? (TV)_fieldInfo.GetValue(instance) : defaultValue;
+  }
+
+  /// <summary>Sets the field value or does nothing if the field is not found.</summary>
+  public void Set(object instance, TV value) {
+    if (_fieldInfo != null) {
+      _fieldInfo.SetValue(instance, value);
+    }
+  }
+}
+
+}
