7c799a4b5cd53923b7401c1b8a51046ab64b7f96
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-01-30 00:21:04 -0800

    Use feature controller for verbose logging

---
+++ TimberCommons/Common/Configurator.cs
@@ -0,0 +1,22 @@
+﻿// Timberborn Mod: Timberborn Commons
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Bindito.Core;
+using TimberApi.ConfiguratorSystem;
+using TimberApi.SceneSystem;
+using UnityDev.Utils.LogUtilsLite;
+
+namespace IgorZ.TimberCommons.Common {
+
+[Configurator(SceneEntrypoint.All)]
+// ReSharper disable once UnusedType.Global
+sealed class Configurator : IConfigurator {
+  public void Configure(IContainerDefinition containerDefinition) {
+    if (Features.DebugExVerboseLogging && DebugEx.LoggingSettings.VerbosityLevel < 5) {
+      DebugEx.LoggingSettings.VerbosityLevel = 5;
+    }
+  }
+}
+
+}
---
+++ TimberCommons/Common/Features.cs
@@ -0,0 +1,29 @@
+﻿// Timberborn Mod: Timberborn Commons
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using IgorZ.TimberDev.Utils;
+using UnityDev.Utils.LogUtilsLite;
+
+namespace IgorZ.TimberCommons.Common {
+
+static class Features {
+  /// <summary>Indicates that <see cref="DebugEx.Fine"/> methods should emit record to the log.</summary>
+  public static bool DebugExVerboseLogging;
+
+  static Features() {
+    FeatureController.ReadFeatures(Consume);
+  }
+
+  static bool Consume(string featureName, bool isEnabled) {
+    switch (featureName) {
+      case "DebugEx.VerboseLogging":
+        DebugExVerboseLogging = isEnabled;
+        return true;
+      default:
+        return false;
+    }
+  }
+}
+
+}
