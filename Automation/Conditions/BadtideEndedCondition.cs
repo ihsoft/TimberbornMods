dc4c8855b4bb3e80dc3cc3a5685c9f95af2e309f
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-02-08 19:11:57 -0800

    Add badtide conditions and templates

---
+++ Automation/Conditions/BadtideEndedCondition.cs
@@ -0,0 +1,38 @@
+// Timberborn Mod: Automation
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Automation.Core;
+
+namespace Automation.Conditions {
+
+/// <summary>Condition that checks if the badtide season has ended.</summary>
+/// <remarks>The state change only happens when a notification is sent from the game.</remarks>
+// ReSharper disable once UnusedType.Global
+public sealed class BadtideEndedCondition : WeatherTrackerConditionBase {
+  const string DescriptionLocKey = "IgorZ.Automation.BadtideEndedCondition.Description";
+
+  #region WeatherTrackerConditionBase implemenantion
+  /// <inheritdoc/>
+  public override string UiDescription => Behavior.Loc.T(DescriptionLocKey);
+
+  /// <inheritdoc/>
+  public override IAutomationCondition CloneDefinition() {
+    return new BadtideEndedCondition();
+  }
+
+  /// <inheritdoc/>
+  public override bool IsValidAt(AutomationBehavior behavior) {
+    return true;
+  }
+
+  /// <inheritdoc/>
+  protected override void OnWeatherChanged(bool? isDrought, bool? isBadtide) {
+    if (isBadtide.HasValue) {
+      ConditionState = !isBadtide.Value;
+    }
+  }
+  #endregion
+}
+
+}
