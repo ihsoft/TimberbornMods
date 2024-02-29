0d2e698f9b8620ccf8e3fc1de523ea4f23d810af
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-02-28 22:22:27 -0800

    Add action code for water regulator

---
+++ Automation/Actions/WaterRegulatorStateAction.cs
@@ -0,0 +1,66 @@
+﻿// Timberborn Mod: Automation
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Automation.Core;
+using Timberborn.Persistence;
+using Timberborn.WaterSourceSystem;
+
+namespace Automation.Actions {
+
+/// <summary>Opens or closes water discharges.</summary>
+public class WaterRegulatorStateAction : AutomationActionBase {
+  const string OpenDescriptionLocKey = "IgorZ.Automation.WaterRegulatorStateActionOpen.Description";
+  const string CloseDescriptionLocKey = "IgorZ.Automation.WaterRegulatorStateActionClose.Description";
+
+  /// <summary>Number of 0.5m steps down from the maximum floodgate height.</summary>
+  // ReSharper disable once MemberCanBePrivate.Global
+  public bool Open { get; private set; }
+
+  #region AutomationActionBase overrides
+
+  /// <inheritdoc/>
+  public override string UiDescription => Behavior.Loc.T(Open ? OpenDescriptionLocKey : CloseDescriptionLocKey);
+
+  /// <inheritdoc/>
+  public override IAutomationAction CloneDefinition() {
+    return new WaterRegulatorStateAction { TemplateFamily = TemplateFamily, Open = Open };
+  }
+
+  /// <inheritdoc/>
+  public override bool IsValidAt(AutomationBehavior behavior) {
+    return behavior.GetComponentFast<WaterSourceRegulator>() != null;
+  }
+
+  /// <inheritdoc/>
+  public override void OnConditionState(IAutomationCondition automationCondition) {
+    if (!Condition.ConditionState) {
+      return;
+    }
+    var regulator = Behavior.GetComponentFast<WaterSourceRegulator>();
+    if (Open) {
+      regulator.Open();
+    } else {
+      regulator.Close();
+    }
+  }
+  #endregion
+
+  #region IGameSerializable implemenation
+  static readonly PropertyKey<bool> OpenPropertyKey = new("Open");
+
+  /// <summary>Loads action state and declaration.</summary>
+  public override void LoadFrom(IObjectLoader objectLoader) {
+    base.LoadFrom(objectLoader);
+    Open = objectLoader.Get(OpenPropertyKey);
+  }
+
+  /// <summary>Saves action state and declaration.</summary>
+  public override void SaveTo(IObjectSaver objectSaver) {
+    base.SaveTo(objectSaver);
+    objectSaver.Set(OpenPropertyKey, Open);
+  }
+  #endregion
+}
+
+}
