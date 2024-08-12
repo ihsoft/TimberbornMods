15ddf1a2cc3b45fd686347c8db385c6226414ca1
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-08-11 23:30:43 -0700

    TimberDev for update 6

---
+++ TimberDev/UI/Configurator.cs
@@ -0,0 +1,19 @@
+﻿// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Bindito.Core;
+
+// ReSharper disable once CheckNamespace
+namespace IgorZ.TimberDev.UI {
+
+[Context("Game")]
+// ReSharper disable once UnusedType.Global
+sealed class Configurator : IConfigurator {
+  public void Configure(IContainerDefinition containerDefinition) {
+    containerDefinition.Bind<UiFactory>().AsSingleton();
+    containerDefinition.Bind<PanelFragment>().AsSingleton();
+  }
+}
+
+}
