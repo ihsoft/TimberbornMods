141e7038433c38d11ea1acd747fb0b8d67253a40
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-09-13 22:44:34 -0700

    Drop towers debug stuff

--- TimberCommons/IrrigationSystem/Configurator.cs
+++ TimberCommons/IrrigationSystem/Configurator.cs
@@ -3,36 +3,21 @@
 // License: Public Domain
 
 using Bindito.Core;
-using IgorZ.TimberDev.Utils;
 using Timberborn.Growing;
 using Timberborn.TemplateSystem;
-using UnityEngine;
 
 namespace IgorZ.TimberCommons.IrrigationSystem {
 
 [Context("Game")]
-// ReSharper disable once UnusedType.Global
 sealed class Configurator : IConfigurator {
-  static readonly string PatchId = typeof(Configurator).AssemblyQualifiedName;
-
   public void Configure(IContainerDefinition containerDefinition) {
     containerDefinition.MultiBind<TemplateModule>().ToProvider(ProvideTemplateModule).AsSingleton();
-    CustomizableInstantiator.AddPatcher(PatchId + "-IrrigationSystem-instantiator", PatchMethod);
   }
 
   static TemplateModule ProvideTemplateModule() {
     var builder = new TemplateModule.Builder();
     builder.AddDecorator<Growable, GrowthRateModifier>();
     return builder.Build();
-  }
-
-  static void PatchMethod(GameObject obj) {
-    PrefabPatcher.AddComponent<GoodConsumingIrrigationTower>(
-        obj,
-        prefab => prefab.name.StartsWith("IrrigationTower."),
-        instance => {
-          instance._irrigationRange = 10;
-        });
   }
 }
 
