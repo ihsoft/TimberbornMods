35d6c91309ba5e0e47ca76607539979cb8bb18bb
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-09-28 22:00:19 -0700

    Create ModTextAssetConverterPatch.cs

---
+++ TimberDev/Utils/ModTextAssetConverterPatch.cs
@@ -0,0 +1,25 @@
+﻿// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System.IO;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace IgorZ.SmartPower.Utils;
+
+/// <summary>Install this patch to improve localization errors logs (there will be a file name).</summary>
+/// <remarks>This patch must be applied from the "MainMenu" context.</remarks>
+[HarmonyPatch]
+static class ModTextAssetConverterPatch {
+  static MethodBase TargetMethod() {
+    return AccessTools.DeclaredMethod("Timberborn.ModdingAssets.ModTextAssetConverter:TryConvert");
+  }
+
+  static void Postfix(FileInfo fileInfo, ref TextAsset asset) {
+    if (fileInfo.Name.EndsWith(".csv")) {
+      asset.name = asset.name + "_at_" + fileInfo.FullName;
+    }
+  }
+}
