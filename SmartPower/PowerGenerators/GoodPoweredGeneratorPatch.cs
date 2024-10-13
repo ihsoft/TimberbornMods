91dd6dc616313ed7ac1766a91c8d1f62fe1cafe7
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2024-10-13 00:03:09 -0700

    Don't cancel the stock logic if not automated

--- SmartPower/PowerGenerators/GoodPoweredGeneratorPatch.cs
+++ SmartPower/PowerGenerators/GoodPoweredGeneratorPatch.cs
@@ -4,6 +4,7 @@
 
 using System.Reflection;
 using HarmonyLib;
+using Timberborn.BaseComponentSystem;
 
 // ReSharper disable UnusedMember.Local
 // ReSharper disable InconsistentNaming
@@ -16,8 +17,11 @@ static class GoodPoweredGeneratorPatch {
     return AccessTools.Method("Timberborn.PowerGenerating.GoodPoweredGenerator:Tick");
   }
 
-  static bool Prefix() {
-    // All power saving logic is run by the SmartPowerService.
-    return false;
+  static bool Prefix(bool __runOriginal, BaseComponent __instance) {
+    if (!__runOriginal) {
+      return false;  // The other patches must follow the same style to properly support the skip logic!
+    }
+    var balancer = __instance.GetComponentFast<PowerOutputBalancer>();
+    return !balancer.Automate;
   }
 }
