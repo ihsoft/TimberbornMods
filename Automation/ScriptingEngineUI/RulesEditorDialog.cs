4c850de2527040d84894fc6415798a1179affd48
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-01-11 16:49:57 -0800

    Mockup of the rules dialog

---
+++ Automation/ScriptingEngineUI/RulesEditorDialog.cs
@@ -0,0 +1,175 @@
+﻿// Timberborn Mod: Automation
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System.Collections.Generic;
+using IgorZ.Automation.ScriptingEngine;
+using IgorZ.TimberDev.UI;
+using TimberApi.UIPresets.Buttons;
+using Timberborn.BaseComponentSystem;
+using Timberborn.SingletonSystem;
+using Timberborn.TooltipSystem;
+using UnityEngine.UIElements;
+
+namespace IgorZ.Automation.ScriptingEngineUI;
+
+sealed class RulesEditorDialog : ILoadableSingleton {
+
+  public List<RuleConstructor> Rules { get; } = [];
+  public VisualElement Content { get; private set; }
+
+  readonly UiFactory _uiFactory;
+  readonly ITooltipRegistrar _tooltipRegistrar;
+  RulesEditorDialog(UiFactory uiFactory, ITooltipRegistrar tooltipRegistrar) {
+    _uiFactory = uiFactory;
+    _tooltipRegistrar = tooltipRegistrar;
+  }
+
+  public void Load() {
+    var addRuleButton = _uiFactory.CreateButton("IgorZ.Automation.Scripting.Editor.AddRuleBtn", AddNewRule);
+    //FIXME: wrap into a scroll view.
+    Content = new VisualElement {
+        style = {
+            minHeight = 600,//FIXME: make it dynamic from the resolution.
+        },
+    };
+    Content.Add(addRuleButton);
+  }
+
+  ConditionConstructor.ConditionDefinition[] _lvalueDefinitions;
+  ActionConstructor.ActionDefinition[] _actionDefinitions;
+
+  public void SetActiveBuilding(BaseComponent building) {
+    var scriptables = new List<IScriptable>();
+    building.GetComponentsFast(scriptables);
+    var conditions = new List<ConditionConstructor.ConditionDefinition>();
+    var actions = new List<ActionConstructor.ActionDefinition>();
+    //FIXME: no 1-to-1, UI has more options.
+    // LOH!
+    // foreach (var scriptable in scriptables) {
+     //  actions.AddRange(scriptable.Actions.Select(x => new ActionConstructor.ActionDefinition {
+      // 	  Action = (x.Name, x.DisplayName),
+      // 	  ArgTypes = x.Arguments.Select(y = > (y.Type, y.Name)).ToArray(),
+     //  }));
+    // }
+  }
+
+  void AddNewRule() {
+    var ruleConstructor = new RuleConstructor(_uiFactory);
+    PopulateTestData(ruleConstructor);
+    Rules.Add(ruleConstructor);
+
+    var wrapper = new VisualElement {
+        style = {
+            flexDirection = FlexDirection.Row,
+            alignItems = Align.Center,
+        },
+    };
+
+    var deleteBtn = _uiFactory.UiBuilder.Create<CrossButton>().BuildAndInitialize();
+    deleteBtn.style.marginRight = 10;
+    _tooltipRegistrar.Register(deleteBtn, _uiFactory.T("IgorZ.Automation.Scripting.Editor.DeleteRuleBtn"));
+    deleteBtn.clicked += () => {
+      Rules.Remove(ruleConstructor);
+      wrapper.RemoveFromHierarchy();
+    };
+    wrapper.Add(deleteBtn);
+    wrapper.Add(ruleConstructor.Root);
+    Content.Insert(Content.childCount - 1, wrapper);
+  }
+
+  //FIXME: make it real!
+  void PopulateTestData(RuleConstructor ruleConstructor) {
+    ruleConstructor.ConditionConstructor.SetDefinitions([
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Weather.IsDrought", "сезон: засуха"),
+            Operands = [
+                Operands.ToDropdownItem(Operands.Type.Equal),
+            ],
+            ArgTypes = [
+                ("True", "true"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Weather.IsBadtide", "сезон: плохая вода"),
+            Operands = [
+                Operands.ToDropdownItem(Operands.Type.Equal),
+            ],
+            ArgTypes = [
+                ("True", "true"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Weather.IsTemperate", "сезон: умеренный"),
+            Operands = [
+                Operands.ToDropdownItem(Operands.Type.Equal),
+            ],
+            ArgTypes = [
+                ("True", "true"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Population.Bots", "население: боты"),
+            Operands = Operands.ToDropdownItems(Operands.All),
+            ArgTypes = [
+                (ArgumentConstructor.NumberTypeName, "значение"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Population.Beavers", "население: бобры"),
+            Operands = Operands.ToDropdownItems(Operands.All),
+            ArgTypes = [
+                (ArgumentConstructor.NumberTypeName, "значение"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Inventory.GoodId.Water", "товар: 'Вода'"),
+            Operands = Operands.ToDropdownItems(Operands.All),
+            ArgTypes = [
+                (ArgumentConstructor.NumberTypeName, "значение"),
+            ],
+        },
+        new ConditionConstructor.ConditionDefinition {
+            Argument = ("Inventory.GoodId.Logs", "товар: 'Дерево'"),
+            Operands = Operands.ToDropdownItems(Operands.All),
+            ArgTypes = [
+                (ArgumentConstructor.NumberTypeName, "значение"),
+            ],
+        },
+    ]);
+
+  //FIXME: get this from the building via interfaces/annotated methods.
+  ruleConstructor.ThenActionConstructor.SetDefinitions([
+      new ActionConstructor.ActionDefinition {
+          Action = ("Floodgate.SetHeight", "выставить высоту затвора в"),
+          ArgTypes = [
+              (ArgumentConstructor.NumberTypeName, "number-loc"),
+          ],
+      },
+      new ActionConstructor.ActionDefinition {
+          Action = ("Pausable.Pause", "остановить работу здания"),
+      },
+      new ActionConstructor.ActionDefinition {
+          Action = ("Pausable.Resume", "продолжить работу здания"),
+      },
+  ]);
+
+  ruleConstructor.ElseActionConstructor.SetDefinitions([
+      new ActionConstructor.ActionDefinition {
+          Action = ("Nothing", "НЕ НАЗНАЧЕНО"),
+      },
+      new ActionConstructor.ActionDefinition {
+          Action = ("Floodgate.SetHeight", "выставить высоту затвора в"),
+          ArgTypes = [
+              (ArgumentConstructor.NumberTypeName, "number-loc"),
+          ],
+      },
+      new ActionConstructor.ActionDefinition {
+          Action = ("Pausable.Pause", "остановить работу здания"),
+      },
+      new ActionConstructor.ActionDefinition {
+          Action = ("Pausable.Resume", "возобновить работу здания"),
+      },
+  ]);
+  }
+}
