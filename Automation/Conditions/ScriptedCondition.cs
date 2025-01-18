960596053387059506a70561ff863097b311ca33
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-01-17 22:17:59 -0800

    Switch scripted condition to the new parser

--- Automation/Conditions/ScriptedCondition.cs
+++ Automation/Conditions/ScriptedCondition.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using IgorZ.Automation.AutomationSystem;
 using IgorZ.Automation.ScriptingEngine;
+using IgorZ.Automation.ScriptingEngine.Parser;
 using Timberborn.Localization;
 using Timberborn.Persistence;
 using UnityDev.Utils.LogUtilsLite;
@@ -32,10 +33,8 @@ sealed class ScriptedCondition : AutomationConditionBase {
 
   /// <inheritdoc/>
   protected override void OnBehaviorAssigned() {
-    try {
-      ParseConditions();
-    } catch (ScriptError e) {
-      HostedDebugLog.Error(Behavior, "Failed to parse conditions: " + e.Message);
+    if (!ParseConditions()) {
+      HostedDebugLog.Error(Behavior, "Failed to parse conditions: {0}", _parserContext.LastError);
       IsMarkedForCleanup = true;
     }
   }
@@ -47,7 +46,7 @@ sealed class ScriptedCondition : AutomationConditionBase {
 
   /// <inheritdoc/>
   public override IAutomationCondition CloneDefinition() {
-    return new ScriptedCondition { Conditions = Conditions };
+    return new ScriptedCondition { Expression = Expression };
   }
 
   /// <inheritdoc/>
@@ -60,153 +59,75 @@ sealed class ScriptedCondition : AutomationConditionBase {
 
   #region API
 
-  /// <summary>Conditions to check.</summary>
+  /// <summary>Script code for expression to check.</summary>
   /// <remarks>
-  /// Conditions are checked for the term "AND". For example, the following string:
-  /// <code>
-  /// [
-  ///   "Weather.Season=drought",
-  ///   "Floodgate.Height>0.5",
-  /// ]
-  /// </code>
-  /// will evaluate to expression: "Weather.Season=Drought AND Floodgate.Height>0.5".
+  /// It must abe a boolean operator. See <see cref="BoolOperatorExpr"/> for the list of conditions. Example of a
+  /// condition: (and (eq (sig Weather.Season) 'drought') (gt Floodgate.Height 0.5)). 
   /// </remarks>
   // ReSharper disable once MemberCanBePrivate.Global
-  public List<string> Conditions { get; private set; }
+  public string Expression { get; private set; }
 
   /// <summary>Sets the expression conditions.</summary>
   /// <remarks>Can only be set on the non-active condition.</remarks>
   /// <seealso cref="Conditions"/>
-  public void SetConditions(IEnumerable<string> operands) {
+  public void SetExpression(string expression) {
     if (Behavior) {
       throw new InvalidOperationException("Cannot change conditions when the behavior is assigned.");
     }
-    Conditions = operands.ToList();
+    Expression = expression;
   }
 
   #endregion
 
   #region IGameSerializable implemenation
 
-  static readonly ListKey<string> ConditionsKey = new("Conditions");
+  static readonly PropertyKey<string> ExpressionKey = new("Expression");
 
   /// <inheritdoc/>
   public override void LoadFrom(IObjectLoader objectLoader) {
     base.LoadFrom(objectLoader);
-    Conditions = objectLoader.Get(ConditionsKey);
+    Expression = objectLoader.Get(ExpressionKey);
   }
 
   /// <inheritdoc/>
   public override void SaveTo(IObjectSaver objectSaver) {
     base.SaveTo(objectSaver);
-    objectSaver.Set(ConditionsKey, Conditions);
+    objectSaver.Set(ExpressionKey, Expression);
   }
 
   #endregion
 
   #region Implementation
 
-  static readonly Regex ConditionRegex = new("([a-zA-Z.]+)([=<>]+)(.+)");
-  readonly List<Operand> _operands = [];
+  ExpressionParser.Context _parserContext;
+  BoolOperatorExpr _parsedExpression;
 
-  void ParseConditions() {
-    var operandsDescriptions = new List<string>();
-    foreach (var serializedOperand in Conditions) {
-      var (name, op, argument) = ParseCondition(serializedOperand);
-
-      var triggerDef = ScriptingService.Instance.GetTriggerDefinition(name, Behavior);
-      var argumentValue = argument;
-      if (triggerDef.ResultType.Options != null) {
-        argumentValue = triggerDef.ResultType.Options
-            .Where(x => x.Value == argument)
-            .Select(x => x.Text)
-            .FirstOrDefault();
-        if (argumentValue == null) {
-          throw new ScriptError($"Cannot find option for '{argument}' in {triggerDef.FullName}");
-        }
+  bool ParseConditions() {
+    _parserContext = new ExpressionParser.Context();
+    _parserContext.OnSignalChanged = CheckOperands;
+    //FIXME: inject it.
+    var parser = new ExpressionParser(ScriptingService.Instance, Behavior);
+    var res = parser.Parse(Expression, _parserContext);
+    //FIXME: process expression to get the descirption
+    _uiDescription = TextColors.ColorizeText($"<SolidHighlight>{Expression}</SolidHighlight>");
+    if (res) {
+      _parsedExpression = _parserContext.ParsedExpression as BoolOperatorExpr;
+      if (_parsedExpression == null) {
+        HostedDebugLog.Error(Behavior, "Expression is not a boolean operator: {0}", Expression);
+        _uiDescription = TextColors.ColorizeText($"<RedHighlight>ERROR</RedHighlight>: ") + _uiDescription;
+        res = false;
       }
-
-      var trigger = ScriptingService.Instance.GetTriggerSource(name, Behavior, CheckOperands);
-      try {
-        var operand = triggerDef.ResultType.ValueType == ScriptValue.TypeEnum.String
-            ? Operand.ForStringArgument(trigger, op, argument)
-            : Operand.ForNumberArgument(trigger, op, argument);
-        _operands.Add(operand);
-      } catch (ScriptError) {
-        trigger.Dispose();
-        throw;
-      }
-      operandsDescriptions.Add($"<SolidHighlight>{triggerDef.DisplayName} {op} {argumentValue}</SolidHighlight>");
     }
-    _uiDescription = TextColors.ColorizeText(string.Join(Behavior.Loc.T(AndOperatorLocString), operandsDescriptions));
+    return res;
   }
 
   void Dispose() {
-    foreach (var op in _operands) {
-      op.TriggerSourceSource.Dispose();
-    }
-    _operands.Clear();
+    _parserContext.Release();
+    _parsedExpression = null;
   }
 
   void CheckOperands() {
-    ConditionState = _operands.All(line => line.Execute());
-  }
-
-  static (string name, string op, string argument) ParseCondition(string operand) {
-    var match = ConditionRegex.Match(operand);
-    if (!match.Success) {
-      throw new ScriptError("Invalid condition: " + operand);
-    }
-    return (match.Groups[1].Value, match.Groups[2].Value, match.Groups[3].Value);
-  }
-
-  #endregion
-
-  #region Warpper for the operand checker
-
-  class Operand {
-    public readonly ITriggerSource TriggerSourceSource;
-    public readonly Func<bool> Execute;
-
-    /// <summary>Creates operand where rvalue is a text constant.</summary>
-    /// <exception cref="ScriptError">if the op is not recognized.</exception>
-    public static Operand ForStringArgument(ITriggerSource triggerSource, string op, string argument) {
-      return new Operand(triggerSource, op, argument);
-    }
-
-    /// <summary>Creates operand where rvalue is a numeric constant.</summary>
-    /// <exception cref="ScriptError">if argument cannot be parsed as number or the op is not recognized.</exception>
-    public static Operand ForNumberArgument(ITriggerSource triggerSource, string op, string argument) {
-      int fixedFloat;
-      try {
-        fixedFloat = Mathf.RoundToInt(float.Parse(argument) * 100);
-      } catch (FormatException) {
-        throw new ScriptError("Cannot parse numeric argument: " + argument);
-      }
-      return new Operand(triggerSource, op, fixedFloat);
-    }
-
-    Operand(ITriggerSource triggerSource, string op, string argument) {
-      TriggerSourceSource = triggerSource;
-      Execute = op switch {
-          "=" => () => triggerSource.CurrentValue.AsString == argument,
-          "<>" => () => triggerSource.CurrentValue.AsString != argument,
-          _ => throw new ScriptError("Unsupported string operand: " + op),
-      };
-    }
-
-    Operand(ITriggerSource triggerSource, string op, int argument) {
-      TriggerSourceSource = triggerSource;
-      Execute = op switch {
-          "=" => () => triggerSource.CurrentValue.AsNumber == argument,
-          "<>" => () => triggerSource.CurrentValue.AsNumber != argument,
-          "<" => () => triggerSource.CurrentValue.AsNumber < argument,
-          "<=" => () => triggerSource.CurrentValue.AsNumber <= argument,
-          ">" => () => triggerSource.CurrentValue.AsNumber > argument,
-          ">=" => () => triggerSource.CurrentValue.AsNumber >= argument,
-          _ => throw new ScriptError("Unsupported numeric operand: " + op),
-      };
-    }
+    ConditionState = _parsedExpression.Execute();
   }
 
   #endregion
