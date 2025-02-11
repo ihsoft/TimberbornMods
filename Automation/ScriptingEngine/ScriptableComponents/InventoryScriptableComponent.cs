b6ad0b56e6d9c14609e894eb664134222625157e
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-02-11 00:56:32 -0800

    Add emptying action to inventory component

--- Automation/ScriptingEngine/ScriptableComponents/InventoryScriptableComponent.cs
+++ Automation/ScriptingEngine/ScriptableComponents/InventoryScriptableComponent.cs
@@ -5,10 +5,15 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Bindito.Core;
+using IgorZ.TimberDev.Utils;
 using Timberborn.BaseComponentSystem;
 using Timberborn.ConstructionSites;
+using Timberborn.Emptying;
 using Timberborn.Goods;
 using Timberborn.InventorySystem;
+using Timberborn.Localization;
+using Timberborn.StatusSystem;
 using UnityDev.Utils.LogUtilsLite;
 
 namespace IgorZ.Automation.ScriptingEngine.ScriptableComponents;
@@ -17,9 +22,16 @@ sealed class InventoryScriptableComponent : ScriptableComponentBase {
 
   const string InputGoodSignalLocKey = "IgorZ.Automation.Scriptable.Inventory.Signal.InputGood";
   const string OutputGoodSignalLocKey = "IgorZ.Automation.Scriptable.Inventory.Signal.OutputGood";
+  const string StartEmptyingStockActionLocKey = "IgorZ.Automation.Scriptable.Inventory.Action.StartEmptyingStock";
+  const string StopEmptyingStockActionLocKey = "IgorZ.Automation.Scriptable.Inventory.Action.StopEmptyingStock";
+  const string EmptyingStatusDescriptionKey = "IgorZ.Automation.Scriptable.Inventory.Action.EmptyingStatus";
 
   const string InputGoodSignalNamePrefix = "Inventory.InputGood.";
   const string OutputGoodSignalNamePrefix = "Inventory.OutputGood.";
+  const string StartEmptyingStockActionName = "Inventory.StartEmptying";
+  const string StopEmptyingStockActionName = "Inventory.StopEmptying";
+
+  const string EmptyingStatusIcon = "IgorZ/status-icon-emptying";
 
   #region ScriptableComponentBase implementation
 
@@ -89,6 +101,30 @@ sealed class InventoryScriptableComponent : ScriptableComponentBase {
   }
 
   /// <inheritdoc/>
+  public override string[] GetActionNamesForBuilding(BaseComponent building) {
+    if (!building.GetComponentFast<Emptiable>()) {
+      return [];
+    }
+    var inventory = GetInventory(building, throwIfNotFound: false);
+    if (inventory.IsOutput
+        && inventory.InputGoods.AsEnumerable().All(input => inventory.OutputGoods.Contains(input))) {
+      // Don't allow emptying buildings with inputs (ingredients) since the input will also be emptied (same as pause).
+      return [StartEmptyingStockActionName, StopEmptyingStockActionName];
+    }
+    return [];
+  }
+  
+  /// <inheritdoc/>
+  public override Action<ScriptValue[]> GetActionExecutor(string name, BaseComponent building) {
+    var emptiable = building.GetComponentFast<Emptiable>();
+    return name switch {
+        StartEmptyingStockActionName => _ => StartEmptyingStockAction(emptiable),
+        StopEmptyingStockActionName => _ => StopEmptyingStockAction(emptiable),
+        _ => throw new ScriptError("Unknown action: " + name),
+    };
+  }
+
+  /// <inheritdoc/>
   public override void RegisterSignalChangeCallback(string name, BaseComponent building, Action onValueChanged) {
     var tracker = building.GetComponentFast<InventoryChangeTracker>()
         ?? _instantiator.AddComponent<InventoryChangeTracker>(building.GameObjectFast);
@@ -103,11 +139,61 @@ sealed class InventoryScriptableComponent : ScriptableComponentBase {
     }
   }
 
+  public override ActionDef GetActionDefinition(string name, BaseComponent _) {
+    return name switch {
+        StartEmptyingStockActionName => StartEmptyingStockActionDef,
+        StopEmptyingStockActionName => StopEmptyingStockActionDef,
+        _ => throw new ScriptError("Unknown action: " + name),
+    };
+  }
+
   #endregion
 
   #region Signals
 
   readonly Dictionary<string, SignalDef> _signalDefs = [];
+
+  #endregion
+
+  #region Actions
+
+  ActionDef StartEmptyingStockActionDef => _startEmptyingStockActionDef ??= new ActionDef {
+      ScriptName = StartEmptyingStockActionName,
+      DisplayName = Loc.T(StartEmptyingStockActionLocKey),
+      Arguments = [],
+  };
+  ActionDef _startEmptyingStockActionDef;
+
+  ActionDef StopEmptyingStockActionDef => _stopEmptyingStockActionDef ??= new ActionDef {
+      ScriptName = StopEmptyingStockActionName,
+      DisplayName = Loc.T(StopEmptyingStockActionLocKey),
+      Arguments = [],
+  };
+  ActionDef _stopEmptyingStockActionDef;
+
+  void StartEmptyingStockAction(Emptiable emptiable) {
+    if (emptiable.IsMarkedForEmptying) {
+      return;
+    }
+    emptiable.MarkForEmptyingWithoutStatus();
+    var status = emptiable.GetComponentFast<EmptyingStatusBehavior>();
+    if (status) {
+      status.RefreshStatus();
+    } else {
+      _instantiator.AddComponent<EmptyingStatusBehavior>(emptiable.GameObjectFast);
+    }
+  }
+  
+  void StopEmptyingStockAction(Emptiable emptiable) {
+    if (!emptiable.IsMarkedForEmptying) {
+      return;
+    }
+    emptiable.UnmarkForEmptying();
+    var status = emptiable.GetComponentFast<EmptyingStatusBehavior>();
+    if (status) {
+      status.RefreshStatus();
+    }
+  }
 
   #endregion
 
@@ -157,6 +243,47 @@ sealed class InventoryScriptableComponent : ScriptableComponentBase {
       foreach (var callback in SignalChangeCallbacks) {
         callback();
       }
+    }
+  }
+
+  #endregion
+
+  #region Emptying status presenter
+
+  /// <summary>
+  /// Creates a custom status icon that indicates that the storage is being emptying. If the status is changed
+  /// externally, then hides the status and notifies the action.
+  /// </summary>
+  sealed class EmptyingStatusBehavior : BaseComponent {
+    StatusToggle _statusToggle;
+    Emptiable _emptiable;
+    ILoc _loc;
+
+    [Inject]
+    public void InjectDependencies(ILoc loc) {
+      _loc = loc;
+    }
+
+    public void RefreshStatus() {
+      if (_emptiable.IsMarkedForEmptying) {
+        _statusToggle.Activate();
+      } else {
+        _statusToggle.Deactivate();
+      }
+    }
+
+    void Start() {
+      _statusToggle = StatusToggle.CreatePriorityStatusWithFloatingIcon(
+          EmptyingStatusIcon, _loc.T(EmptyingStatusDescriptionKey));
+      GetComponentFast<StatusSubject>().RegisterStatus(_statusToggle);
+
+      _emptiable = GetComponentFast<Emptiable>();
+      _emptiable.UnmarkedForEmptying += OnUnmarkedForEmptying;
+      RefreshStatus();
+    }
+
+    void OnUnmarkedForEmptying(object sender, EventArgs args) {
+      _statusToggle.Deactivate();
     }
   }
 
