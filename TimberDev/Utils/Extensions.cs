d623759f79c420394bfaaaa26c33168cb66a4278
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-02-11 00:37:12 -0800

    Add extensions util for readonly lists

---
+++ TimberDev/Utils/Extensions.cs
@@ -0,0 +1,19 @@
+﻿// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System.Collections.Generic;
+using Timberborn.Common;
+
+// ReSharper disable once CheckNamespace
+namespace IgorZ.TimberDev.Utils;
+
+static class Extensions {
+  /// <summary>Converts readonly hash set to enumerable (for using in LINQ expressions).</summary>
+  public static IEnumerable<T> AsEnumerable<T>(this ReadOnlyHashSet<T> enumerator) {
+    using var it = enumerator.GetEnumerator();
+    while (it.MoveNext()) {
+      yield return it.Current;
+    }
+  }
+}
