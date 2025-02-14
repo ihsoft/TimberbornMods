659402d85f98e723ccc3fa1eb3782daa9e0a4b10
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-02-13 21:54:21 -0800

    Create no TAPI version of UI Factory

--- TimberDev/UI/UiFactory.cs
+++ TimberDev/UI/UiFactory.cs
@@ -1,38 +1,46 @@
-// Timberborn Utils
+﻿// Timberborn Utils
 // Author: igor.zavoychinskiy@gmail.com
 // License: Public Domain
 
 using System;
 using System.Runtime.CompilerServices;
-using TimberApi.UIBuilderSystem;
-using TimberApi.UIBuilderSystem.StylingElements;
-using TimberApi.UIPresets.Buttons;
-using TimberApi.UIPresets.Labels;
-using TimberApi.UIPresets.Sliders;
-using TimberApi.UIPresets.Toggles;
+using Timberborn.AssetSystem;
 using Timberborn.CoreUI;
 using Timberborn.Localization;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-// ReSharper disable MemberCanBePrivate.Global
 namespace IgorZ.TimberDev.UI;
 
-/// <summary>Factory for making standard fragment panel elements.</summary>
-public sealed class UiFactory {
+/// <summary>Factory for creating UI elements without using TAPI.</summary>
+/// <remarks>Normally, should be used for the times when TAPI is broken.</remarks>
+public class UiFactory {
   readonly VisualElementLoader _visualElementLoader;
   readonly ILoc _loc;
+  readonly IAssetLoader _assetLoader;
 
   /// <summary>Common padding around the button text on the right side panel.</summary>
-  public static readonly Padding StandardButtonPadding = new(2, 10, 2, 10);
+  public static readonly (int top, int left, int bottom, int right) StandardButtonPadding = new(2, 10, 2, 10);
 
-  /// <summary>The TAPI UI builder.</summary>
-  public readonly UIBuilder UiBuilder;
+  /// <summary>Default color for the text in UI.</summary>
+  public static readonly Color DefaultColor = new(0.8f, 0.8f, 0.8f);
 
-  UiFactory(VisualElementLoader visualElementLoader, UIBuilder uiBuilder, ILoc loc) {
+  /// <summary>Find the specified element starting from the parent and upstream.</summary>
+  public static T FindElementUpstream<T>(VisualElement root, string name) where T : VisualElement {
+    while (root != null) {
+      var element = root.Q<T>(name);
+      if (element != null) {
+        return element;
+      }
+      root = root.parent;
+    }
+    throw new InvalidOperationException($"Element '{name}' not found.");
+  }
+
+  UiFactory(VisualElementLoader visualElementLoader, ILoc loc, IAssetLoader assetLoader) {
     _visualElementLoader = visualElementLoader;
-    UiBuilder = uiBuilder;
     _loc = loc;
+    _assetLoader = assetLoader;
   }
 
   /// <summary>A shortcut to "ILoc.T()".</summary>
@@ -51,10 +59,81 @@ public sealed class UiFactory {
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public string T<T1, T2, T3>(string key, T1 param1, T2 param2, T3 param3) => _loc.T(key, param1, param2, param3);
 
+  /// <summary>Stylesheet for the Timberborn UI.</summary>
+  public StyleSheet TimberbornStylesheet =>
+      _timberbornStylesheet ??= _assetLoader.Load<StyleSheet>("UI/Views/TimberbornStyle");
+  StyleSheet _timberbornStylesheet;
+
+  /// <summary>Creates a panel builder that can be used as a fragment on the right side panel.</summary>
+  public VisualElement CreateCenteredPanelFragment() {
+    var root = _visualElementLoader.LoadVisualElement("Game/EntityPanel/BatteryFragment");
+    root.Clear();
+    return root;
+  }
+
+  /// <summary>Wraps the element to make it centered in the UI fragment.</summary>
+  public VisualElement CenterElement(VisualElement element) {
+    var center = new VisualElement();
+    center.style.justifyContent = Justify.Center;
+    center.style.flexDirection = FlexDirection.Row;
+    center.Add(element);
+    return center;
+  }
+
+  /// <summary>Creates a label in a theme suitable for the right side panel.</summary>
+  /// <param name="locKey">Optional loc key for the caption.</param>
+  public Label CreateLabel(string locKey = null) {
+    var label = _visualElementLoader.LoadVisualElement("Game/EntityPanel/SluiceFragment").Q<Label>("DepthLabel");
+    label.RemoveFromHierarchy();
+    if (locKey != null) {
+      label.text = T(locKey);
+    }
+    return label;
+  }
+
+  /// <summary>Creates a button in a theme suitable for the right side panel.</summary>
+  /// <param name="locKey">Loc key for the caption.</param>
+  /// <param name="onClickFn">Callback to call when the button is clicked.</param>
+  /// <param name="padding">
+  /// Optional padding around the button text. If not set, then <see cref="StandardButtonPadding"/> will be used.
+  /// </param>
+  public Button CreateButton(string locKey, Action onClickFn,
+                             (int top, int left, int bottom, int right)? padding = null) {
+    var button = _visualElementLoader.LoadVisualElement("Game/EntityPanel/DebugFragment").Q<Button>("Button");
+    button.RemoveFromHierarchy();
+    button.text = T(locKey);
+    button.style.marginTop = 0;
+    button.style.fontSize = 14;
+    button.style.color = Color.white;
+    var setPadding = padding ?? StandardButtonPadding;
+    button.style.paddingTop = setPadding.top;
+    button.style.paddingLeft = setPadding.left;
+    button.style.paddingBottom = setPadding.bottom;
+    button.style.paddingRight = setPadding.right;
+    button.clicked += onClickFn;
+    return button;
+  }
+
+  /// <summary>Creates a toggle in a theme suitable for the right side panel.</summary>
+  /// <param name="locKey">Loc key for the caption. If null or empty, then there will be no caption.</param>
+  /// <param name="onValueChangedFn">
+  /// A callback method that will be called on the value change. The only argument is the new value.
+  /// </param>
+  /// <param name="spacing">If a non-negative value, then the bottom margin will be set.</param>
+  public Toggle CreateToggle(string locKey, Action<ChangeEvent<bool>> onValueChangedFn, int spacing = 5) {
+    var toggle = _visualElementLoader.LoadVisualElement("Game/EntityPanel/HaulCandidateFragment").Q<Toggle>("Toggle");
+    toggle.RemoveFromHierarchy();
+    if (locKey != null) {
+      toggle.text = T(locKey);
+    }
+    toggle.RegisterValueChangedCallback(evt => onValueChangedFn(evt));
+    if (spacing >= 0) {
+      toggle.style.marginBottom = spacing;
+    }
+    return toggle;
+  }
+
   /// <summary>Creates a slider in a theme suitable for the right side panel.</summary>
-  /// <remarks>
-  /// TAPI offers the sliders builder, but in the recent updates it got broken. Use this factory as a quick workaround.
-  /// </remarks>
   /// <param name="onValueChangedFn">
   /// A callback method that will be called on the value change. The only argument is the new value.
   /// </param>
@@ -66,7 +145,9 @@ public sealed class UiFactory {
   /// <param name="spacing">If a non-negative value, then the bottom margin will be set.</param>
   public Slider CreateSlider(Action<ChangeEvent<float>> onValueChangedFn, float lowValue, float highValue,
                              float stepSize = 0, int spacing = 5) {
-    var slider = UiBuilder.Create<GameTextSlider>().Small().Build();
+    var slider = _visualElementLoader.LoadVisualElement("Game/EntityPanel/BatteryFragment").Q<Slider>("ChargeSlider");
+    slider.RemoveFromHierarchy();
+    slider.style.marginTop = 0;
     slider.lowValue = lowValue;
     slider.highValue = highValue;
     slider.RegisterValueChangedCallback(
@@ -85,18 +166,20 @@ public sealed class UiFactory {
   }
 
   /// <summary>Creates a min/max slider in a theme suitable for the right side panel.</summary>
+  /// <remarks>
+  /// For this method to work, "TimberbornStyle.uss" should be compiled into the mod asset bundle at path
+  /// "UI/Views/TimberbornStyle".
+  /// </remarks>
   /// <param name="onValueChangedFn">A callback method that will be called on the value change.</param>
   /// <param name="lowValue">The minimum value limit.</param>
   /// <param name="highValue">The maximum value limit.</param>
   /// <param name="minDelta">The minimum delta between min/max values.</param>
   /// <param name="stepSize">If greater than zero, then the values are rounded to the step.</param>
   /// <param name="spacing">If a non-negative value, then the bottom margin will be set.</param>
-  public MinMaxSlider2 CreateMinMaxSlider(Action<ChangeEvent<Vector2>> onValueChangedFn, float lowValue, float highValue,
-                                         float minDelta, float stepSize = 0, int spacing = 5) {
-    var slider = UiBuilder.Create<GameTextMinMaxSlider2>()
-        .SetLowLimit(lowValue)
-        .SetHighLimit(highValue)
-        .Small().Build();
+  public MinMaxSlider CreateMinMaxSlider(Action<ChangeEvent<Vector2>> onValueChangedFn, float lowValue, float highValue,
+                                          float minDelta, float stepSize = 0, int spacing = 5) {
+    var slider = new MinMaxSlider(lowValue, highValue, lowValue, highValue);
+    slider.styleSheets.Add(TimberbornStylesheet);
     slider.RegisterValueChangedCallback(
         evt => {
           var newValue = evt.newValue;
@@ -135,71 +218,10 @@ public sealed class UiFactory {
   public PreciseSliderWrapper CreatePreciseSlider(float stepSize, Action<float> onValueChangedFn, int spacing = 5) {
     var root = _visualElementLoader.LoadVisualElement("Game/EntityPanel/SluiceFragment");
     var slider = root.Q<PreciseSlider>("WaterLevelSlider");
+    slider.RemoveFromHierarchy();
     if (spacing >= 0) {
       slider.style.marginBottom = spacing;
     }
     return new PreciseSliderWrapper(slider, onValueChangedFn, stepSize);
-  }
-
-  /// <summary>Creates a toggle in a theme suitable for the right side panel.</summary>
-  /// <param name="locKey">Loc key for the caption. If null or empty, then there will be no caption.</param>
-  /// <param name="onValueChangedFn">
-  /// A callback method that will be called on the value change. The only argument is the new value.
-  /// </param>
-  /// <param name="spacing">If a non-negative value, then the bottom margin will be set.</param>
-  public Toggle CreateToggle(string locKey, Action<ChangeEvent<bool>> onValueChangedFn, int spacing = 5) {
-    var toggle = !string.IsNullOrEmpty(locKey)
-        ? UiBuilder.Create<GameToggle>().SetLocKey(locKey).Build()
-        : UiBuilder.Create<GameTextToggle>().Build();
-    toggle.RegisterValueChangedCallback(evt => onValueChangedFn(evt));
-    if (spacing >= 0) {
-      toggle.style.marginBottom = spacing;
-    }
-    return toggle;
-  }
-
-  /// <summary>Creates a label in a theme suitable for the right side panel.</summary>
-  /// <param name="locKey">Optional loc key for the caption.</param>
-  public Label CreateLabel(string locKey = null) {
-    return UiBuilder.Create<GameTextLabel>().SetText(locKey != null ? T(locKey) : "").Build();
-  }
-
-  /// <summary>Creates a button in a theme suitable for the right side panel.</summary>
-  /// <param name="locKey">Loc key for the игеещт caption.</param>
-  /// <param name="onClickFn">Callback to call when the button is clicked.</param>
-  /// <param name="padding">
-  /// Optional padding around the button text. If not set, then <see cref="StandardButtonPadding"/> will be used.
-  /// </param>
-  public Button CreateButton(string locKey, Action onClickFn, Padding? padding = null) {
-    var button = UiBuilder.Create<GameButton>()
-        .SetLocKey(locKey)
-        .ModifyRoot(builder => builder.SetPadding(padding ?? StandardButtonPadding))
-        .Build();
-    button.clicked += onClickFn;
-    return button;
-  }
-
-  /// <summary>Wraps the element to make it centered in the UI fragment.</summary>
-  public VisualElement CenterElement(VisualElement element) {
-    var center = new VisualElement {
-        style = {
-            justifyContent = Justify.Center,
-            flexDirection = FlexDirection.Row
-        }
-    };
-    center.Add(element);
-    return center;
-  }
-
-  /// <summary>Creates a panel builder that can be used as a fragment on the right side panel.</summary>
-  /// <remarks>
-  /// This is a root element for the fragment's panel. Add controls to it via
-  /// <see cref="PanelFragmentBuilder{PanelFragment}.AddComponent(UnityEngine.UIElements.VisualElement)"/>.
-  /// </remarks>
-  public PanelFragment CreateCenteredPanelFragmentBuilder() {
-    return UiBuilder.Create<PanelFragment>()
-        .SetFlexDirection(FlexDirection.Column)
-        .SetWidth(new Length(100f, LengthUnit.Percent))
-        .SetJustifyContent(Justify.Center);
   }
 }
