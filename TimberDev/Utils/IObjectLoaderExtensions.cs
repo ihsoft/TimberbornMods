404925bd45e8d6a668fdd1f00642e594c7023ebb
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-02-13 21:47:59 -0800

    Bring back the deprecated methods

---
+++ TimberDev/Utils/IObjectLoaderExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Timberborn.Persistence;
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace IgorZ.TimberDev.Utils;
+
+public static class IObjectLoaderExtensions {
+  public static bool? GetValueOrNullable(this IObjectLoader objectLoader, PropertyKey<bool> key) {
+    return objectLoader.Has(key) ? objectLoader.Get(key) : null;
+  }
+
+  public static int? GetValueOrNullable(this IObjectLoader objectLoader, PropertyKey<int> key) {
+    return objectLoader.Has(key) ? objectLoader.Get(key) : null;
+  }
+
+  public static float? GetValueOrNullable(this IObjectLoader objectLoader, PropertyKey<float> key) {
+    return objectLoader.Has(key) ? objectLoader.Get(key) : null;
+  }
+
+  public static string GetValueOrNullable(this IObjectLoader objectLoader, PropertyKey<string> key) {
+    return objectLoader.Has(key) ? objectLoader.Get(key) : null;
+  }
+}
