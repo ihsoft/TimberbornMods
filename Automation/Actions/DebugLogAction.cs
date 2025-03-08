35ffcf60047640db2508aabbc1ff7825afa47261
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-03-07 18:59:38 -0800

    Create DebugLogAction.cs

---
+++ Automation/Actions/DebugLogAction.cs
@@ -0,0 +1,39 @@
+﻿// Timberborn Mod: Automation
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using IgorZ.Automation.AutomationSystem;
+using Timberborn.Localization;
+using UnityDev.Utils.LogUtilsLite;
+
+namespace IgorZ.Automation.Actions;
+
+/// <summary>Writes a debug message into the log.</summary>
+public class DebugLogAction : AutomationActionBase {
+
+  #region AutomationActionBase overrides
+
+  /// <inheritdoc/>
+  public override IAutomationAction CloneDefinition() {
+    return new DebugLogAction { TemplateFamily = TemplateFamily };
+  }
+
+  /// <inheritdoc/>
+  public override string UiDescription =>
+      TextColors.ColorizeText("<SolidHighlight>write 'hello' into the log</SolidHighlight>");
+
+  /// <inheritdoc/>
+  public override bool IsValidAt(AutomationBehavior behavior) {
+    return true;
+  }
+
+  /// <inheritdoc/>
+  public override void OnConditionState(IAutomationCondition automationCondition) {
+    if (!Condition.ConditionState) {
+      return;
+    }
+    HostedDebugLog.Warning(Behavior, "[DebugLogAction] HELLO!");
+  }
+
+  #endregion
+}
