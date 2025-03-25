08d41ae7ee181fdabab3e1cb2b5e4d4060110155
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-03-24 20:01:23 -0700

    Refactor automation fragment

--- Automation/UI/AutomationFragment.cs
+++ Automation/UI/AutomationFragment.cs
@@ -2,34 +2,35 @@
 // Author: igor.zavoychinskiy@gmail.com
 // License: Public Domain
 
-using System.Collections.Generic;
-using System.Text;
-using IgorZ.Automation.Actions;
 using IgorZ.Automation.AutomationSystem;
-using IgorZ.Automation.Conditions;
 using IgorZ.Automation.ScriptingEngineUI;
 using IgorZ.TimberDev.UI;
 using Timberborn.BaseComponentSystem;
 using Timberborn.CoreUI;
 using Timberborn.EntityPanelSystem;
-using UnityEngine;
+using UnityDev.Utils.LogUtilsLite;
 using UnityEngine.UIElements;
 
 namespace IgorZ.Automation.UI;
 
 sealed class AutomationFragment : IEntityPanelFragment {
 
-  const string RulesAreaCaptionTextLocKey = "IgorZ.Automation.AutomationFragment.RulesAreaCaptionTextLocKey";
   const string RuleTextLocKey = "IgorZ.Automation.AutomationFragment.RuleTextLocKey";
-  const string AddRulesBtnCaptionLocKey = "IgorZ.Automation.AutomationFragment.AddRulesBtn";
-  const string EditRulesBtnCaptionLocKey = "IgorZ.Automation.AutomationFragment.EditRulesBtn";
+  const string RuleRowTemplate = "IgorZ.Automation/FragmentRule";
+  const string FragmentResource = "IgorZ.Automation/EntityPanelFragment";
+
+  const string RulesPanelName = "RulesPanel";
+  const string RulesListName = "RulesList";
+  const string RulesAdjustmentControlsName = "RulesAdjustmentControls";
+  const string AddRulesButtonName = "AddRulesButton";
+  const string EditRulesButtonName = "EditRulesButton";
+  const string CopyRulesButtonName = "CopyRulesButton";
 
   readonly UiFactory _uiFactory;
   readonly RulesEditorDialog _rulesEditorDialog;
 
   VisualElement _root;
-  Label _caption;
-  Label _rulesList;
+  VisualElement _rulesList;
   Button _addRulesButton;
 
   AutomationBehavior _automationBehavior;
@@ -40,18 +41,17 @@ sealed class AutomationFragment : IEntityPanelFragment {
   }
 
   public VisualElement InitializeFragment() {
-    _caption = _uiFactory.CreateLabel();
-    _caption.style.color = Color.cyan;
+    _root = _uiFactory.LoadVisualElement(FragmentResource);
+    _rulesList = _root.Q(RulesListName);
 
-    _rulesList = _uiFactory.CreateLabel();
+    _addRulesButton = _root.Q<Button>(AddRulesButtonName);
+    _addRulesButton.clicked += () => _rulesEditorDialog.Show(_automationBehavior, UpdateView);
+    _root.Q<Button>(EditRulesButtonName).clicked += () => _rulesEditorDialog.Show(_automationBehavior, UpdateView);
+    _root.Q<Button>(CopyRulesButtonName).clicked += () => {
+      //FIXME
+      DebugEx.Warning("*** Copying rules is not implemented yet.");
+    };
 
-    _addRulesButton = _uiFactory.CreateButton(
-            AddRulesBtnCaptionLocKey, _ => _rulesEditorDialog.Show(_automationBehavior, UpdateView));
-
-    _root = _uiFactory.CreateCenteredPanelFragment();
-    _root.Add(_caption);
-    _root.Add(_rulesList);
-    _root.Add(_addRulesButton);
     _root.ToggleDisplayStyle(visible: false);
     return _root;
   }
@@ -74,35 +74,17 @@ sealed class AutomationFragment : IEntityPanelFragment {
       return;
     }
     _root.ToggleDisplayStyle(visible: true);
-    _caption.ToggleDisplayStyle(_automationBehavior.HasActions);
-    _rulesList.ToggleDisplayStyle(_automationBehavior.HasActions);
+    _root.Q(RulesPanelName).ToggleDisplayStyle(_automationBehavior.HasActions);
+    _root.Q(RulesAdjustmentControlsName).ToggleDisplayStyle(_automationBehavior.HasActions);
+    _addRulesButton.ToggleDisplayStyle(!_automationBehavior.HasActions);
     if (!_automationBehavior.HasActions) {
-      _addRulesButton.text = _uiFactory.T(AddRulesBtnCaptionLocKey);
       return;
     }
-    _addRulesButton.text = _uiFactory.T(EditRulesBtnCaptionLocKey);
-    var sortedActions = new List<IAutomationAction>();
+    _rulesList.Clear();
     foreach (var action in _automationBehavior.Actions) {
-      var insertPos = 0;
-      while (insertPos < sortedActions.Count) {
-        if (string.CompareOrdinal(sortedActions[insertPos].TemplateFamily, action.TemplateFamily) > 0) {
-          break;
-        }
-        insertPos++;
-      }
-      sortedActions.Insert(insertPos, action);
+      var row = _uiFactory.LoadVisualElement(RuleRowTemplate);
+      row.Q<Label>("Content").text = _uiFactory.T(RuleTextLocKey, action.Condition.UiDescription, action.UiDescription);
+      _rulesList.Add(row);
     }
-    var rules = new StringBuilder();
-    var actionsAdded = 0;
-    foreach (var action in sortedActions) {
-      if (actionsAdded++ > 0) {
-        rules.AppendLine();
-      }
-      rules.Append(SpecialStrings.RowStarter);
-      rules.Append(_uiFactory.T(RuleTextLocKey, action.Condition.UiDescription, action.UiDescription));
-    }
-    _caption.text = _uiFactory.T(RulesAreaCaptionTextLocKey);
-    _rulesList.text = rules.ToString();
-    _root.ToggleDisplayStyle(visible: true);
   }
 }
