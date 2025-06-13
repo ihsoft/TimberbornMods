172181f93c3f2b5187f0e150b116e1ea0f9b7909
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-06-13 16:22:38 -0700

    Add a base settings class

---
+++ TimberDev/Settings/BaseSettings.cs
@@ -0,0 +1,53 @@
+﻿// Timberborn Utils
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using System;
+using System.Collections.Generic;
+using ModSettings.Core;
+using Timberborn.Modding;
+using Timberborn.SettingsSystem;
+
+// ReSharper disable once CheckNamespace
+namespace IgorZ.TimberDev.Settings;
+
+/// <summary>Base class for teh mod settings.</summary>
+/// <remarks>
+/// Inherit from this class and add an injection in the configurator. Use
+/// <see cref="InstallSettingCallback{TV}(ModSetting{TV},Action{TV})"/> to create a static property that will update to
+/// the setting value.
+/// </remarks>
+/// <typeparam name="T"></typeparam>
+abstract class BaseSettings<T> : ModSettingsOwner where T : BaseSettings<T> {
+
+  protected static T Instance;
+
+  /// <summary>Set up a callback that will get called on the setting update and on the game load.</summary>
+  protected void InstallSettingCallback<TV>(ModSetting<TV> setting, Action<TV> setter) where TV : notnull {
+    setting.ValueChanged += (_, _) => setter(setting.Value);
+    _afterLoadActions.Add(() => setter(setting.Value));
+  }
+
+  /// <inheritdoc cref="InstallSettingCallback{TV}(ModSetting{TV}, Action{TV})"/>
+  protected void InstallSettingCallback<TV>(ModSetting<TV> setting, Action setter) where TV : notnull {
+    setting.ValueChanged += (_, _) => setter();
+    _afterLoadActions.Add(setter);
+  }
+
+  /// <inheritdoc />
+  protected override void OnAfterLoad() {
+    base.OnAfterLoad();
+    foreach (var action in _afterLoadActions) {
+      action();
+    }
+    _afterLoadActions.Clear();
+  }
+
+  readonly List<Action> _afterLoadActions = [];
+
+  protected BaseSettings(
+      ISettings settings, ModSettingsOwnerRegistry modSettingsOwnerRegistry, ModRepository modRepository) :
+      base(settings, modSettingsOwnerRegistry, modRepository) {
+    Instance = (T)this;
+  }
+}
