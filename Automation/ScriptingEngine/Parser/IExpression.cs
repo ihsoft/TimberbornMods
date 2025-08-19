f09b93b110156550ab05e7f404f9a6321814cc17
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-08-18 20:01:05 -0700

    Update IExpression.cs

--- Automation/ScriptingEngine/Parser/IExpression.cs
+++ Automation/ScriptingEngine/Parser/IExpression.cs
@@ -7,7 +7,12 @@ using System;
 namespace IgorZ.Automation.ScriptingEngine.Parser;
 
 interface IExpression {
+  /// <summary>Returns a string that can be parsed back to the rule.</summary>
   public string Serialize();
+
+  /// <summary>Returns a human-friendly description of the expression.</summary>
   public string Describe();
+
+  /// <summary>Visits all nodes in the expression tree and applies the visitor function to each node.</summary>
   public void VisitNodes(Action<IExpression> visitorFn);
 }
