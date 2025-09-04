86897554ee03a4be7f28c28aaaf860ca30ddf5cc
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-09-03 23:48:51 -0700

    Support assemblies of any number of parts in the name

--- UnityDevLite/TypeUtils.cs
+++ UnityDevLite/TypeUtils.cs
@@ -3,29 +3,40 @@
 // License: Public Domain
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using UnityDev.Utils.LogUtilsLite;
 
 // ReSharper disable once CheckNamespace
 namespace UnityDev.Utils.Reflections;
 
 /// <summary>Various utils to deal with the types via reflections.</summary>
 static class TypeUtils {
-  /// <summary>Gets the type by it's full name. Even if the type is internal.</summary>
-  /// <exception cref="ArgumentException">if the type cannot be found in the current appdomain.</exception>
-  public static Type GetInternalType(string fullTypeName) {
-    var assemblyParts = fullTypeName.Split(new[] { '.' }, 3);
-    if (assemblyParts.Length < 3) {
-      throw new ArgumentException($"Invalid full type name: {fullTypeName}");
+  /// <summary>Gets the type by its full name. Even if the type is internal.</summary>
+  /// <exception cref="ArgumentException">if the type can't be found in the current appdomain.</exception>
+  public static Type GetInternalType(string fullTypeName, bool throwIfNotFound = true) {
+    var assemblyParts = fullTypeName.Split('.');
+    var assemblyNameCandidates = new List<string>();
+    for (var i = 1; i < assemblyParts.Length - 1; i++) {
+      var assemblyName = string.Join(".", assemblyParts, 0, i);
+      assemblyNameCandidates.Add(assemblyName);
     }
-    var assemblyName = $"{assemblyParts[0]}.{assemblyParts[1]}";
-    var assembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName);
+    var assembly = AppDomain.CurrentDomain.GetAssemblies()
+        .FirstOrDefault(x => assemblyNameCandidates.Contains(x.GetName().Name));
     if (assembly == null) {
-      throw new ArgumentException($"Cannot find assembly: {assemblyName}");
+      if (throwIfNotFound) {
+        throw new ArgumentException($"Cannot find assembly for type: {fullTypeName}");
+      }
+      return null;
     }
     var type = assembly.GetType(fullTypeName);
-    if (type == null) {
+    if (type != null) {
+      return type;
+    }
+    if (throwIfNotFound) {
       throw new ArgumentException($"Cannot get type 'fullTypeName' from assembly {assembly}");
     }
-    return type;
+    DebugEx.Warning("Cannot get type '{0}' from assembly {1}", fullTypeName, assembly);
+    return null;
   }
 }
