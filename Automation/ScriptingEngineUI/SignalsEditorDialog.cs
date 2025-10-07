71ebf21586992a5fd59ed455440717d06684fdf4
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2025-10-06 22:38:05 -0700

    Handle signal saving

--- Automation/ScriptingEngineUI/SignalsEditorDialog.cs
+++ Automation/ScriptingEngineUI/SignalsEditorDialog.cs
@@ -3,13 +3,20 @@
 // License: Public Domain
 
 using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using IgorZ.TimberDev.UI;
 using Timberborn.CoreUI;
+using UnityDev.Utils.LogUtilsLite;
 using UnityEngine.UIElements;
 
 namespace IgorZ.Automation.ScriptingEngineUI;
 
 sealed class SignalsEditorDialog : IPanelController {
+
+  const string SignalsEditorDialogAsset = "IgorZ.Automation/SignalsEditor";
+  const string SignalSourceTmplAsset = "IgorZ.Automation/SignalSourceTmpl";
+  const string SignalTargetTmplAsset = "IgorZ.Automation/SignalTargetTmpl";
 
   #region IPanelController implementation
 
@@ -25,30 +32,42 @@ sealed class SignalsEditorDialog : IPanelController {
   }
 
   void SaveAndClose() {
+    //FIXME: check fro the right custom signal name
+    foreach (var exportedSignal in _exportedSignals) {
+      if (exportedSignal.customSignalField.value == "") {
+        continue;
+      }
+      if (!ValidateCustomSignalName(exportedSignal.customSignalField.value)) {
+        return;
+      }
+    }
+
+    _rulesUiHelper.ClearSignalsOnBuilding();
+    foreach (var exportedSignal in _exportedSignals) {
+      if (exportedSignal.customSignalField.value == "") {
+        continue;
+      }
+      _rulesUiHelper.SetExportedSignalName(exportedSignal.buildingSignalName, exportedSignal.customSignalField.value);
+    }
     Close();
-    _onSave?.Invoke();
   }
 
   /// <inheritdoc/>
   public void OnUICancelled() {
     Close();
   }
-  Action _onSave;
 
   #endregion
 
   #region API
 
-  const string SignalsEditorDialogAsset = "IgorZ.Automation/SignalsEditor";
-
-  VisualElement _rowsContainer;
   ScriptingRulesUIHelper _rulesUiHelper;
+  readonly List<(string buildingSignalName, TextField customSignalField)> _exportedSignals = [];
 
   /// <summary>Show this dialog. It blocks the game.</summary>
   public void Show(ScriptingRulesUIHelper rulesUIHelper, Action onSave) {
     _rulesUiHelper = rulesUIHelper;
     _root = _uiFactory.LoadVisualTreeAsset(SignalsEditorDialogAsset);
-    _rowsContainer = _root.Q2<VisualElement>("RowsContainer");
     _root.Q<Button>("ConfirmButton").clicked += () => OnUIConfirmed();
     _root.Q<Button>("CancelButton").clicked += Close;
     _root.Q<Button>("CloseButton").clicked += Close;
@@ -57,16 +76,20 @@ sealed class SignalsEditorDialog : IPanelController {
     sourceSection.Clear();
     var targetSection = _root.Q2<VisualElement>("TargetsSection");
     targetSection.Clear();
-    foreach (var signal in _rulesUiHelper.BuildingSignals) {
-      var sourceTmpl = _uiFactory.LoadVisualElement("IgorZ.Automation/SignalSourceTmpl");
-      sourceTmpl.Q2<Label>("SignalName").text = signal.Describe;
+    _exportedSignals.Clear();
+    foreach (var signalMapping in _rulesUiHelper.BuildingSignals) {
+      var sourceTmpl = _uiFactory.LoadVisualElement(SignalSourceTmplAsset);
+      sourceTmpl.Q2<Label>("SignalName").text = signalMapping.Describe;
       sourceSection.Add(sourceTmpl);
-      var targetTmpl = _uiFactory.LoadVisualElement("IgorZ.Automation/SignalTargetTmpl");
-      targetTmpl.Q2<TextField>("SignalName").value = signal.ExportedSignalName ?? "";
+
+      var targetTmpl = _uiFactory.LoadVisualElement(SignalTargetTmplAsset);
+      var targetField = targetTmpl.Q2<TextField>("SignalName");
+      targetField.value = signalMapping.ExportedSignalName ?? "";
+      targetTmpl.Q2<Button>("ClearSignalButton").clicked += () => targetField.value = "";
       targetSection.Add(targetTmpl);
+      _exportedSignals.Add((signalMapping.SignalName, targetField));
     }
 
-    _onSave = onSave;
     _panelStack.PushDialog(this);
   }
 
@@ -77,6 +100,8 @@ sealed class SignalsEditorDialog : IPanelController {
 
   #region Implementation
 
+  static readonly Regex MappedSignalNamePattern = new(@"^(?!\.)([A-Za-z][A-Za-z0-9]+\.?)*([A-Za-z][A-Za-z0-9]*)$");
+
   VisualElement _root;
   
   readonly UiFactory _uiFactory;
@@ -85,6 +110,15 @@ sealed class SignalsEditorDialog : IPanelController {
   SignalsEditorDialog(UiFactory uiFactory, PanelStack panelStack) {
     _uiFactory = uiFactory;
     _panelStack = panelStack;
+  }
+
+  bool ValidateCustomSignalName(string name) {
+    //FIXME: Don't log bad names. Highlight via UI.
+    if (!MappedSignalNamePattern.IsMatch(name)) {
+      DebugEx.Error("Invalid signal name: {0}", name);
+      return false;
+    }
+    return true;
   }
 
   #endregion
