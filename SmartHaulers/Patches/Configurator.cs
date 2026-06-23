21a664efd29a8dc8ebbd9130cd7c1b1bcd12612c
Igor Zavoychinskiy <igor.zavoychinskiy@gmail.com>
2026-06-23 00:05:03 -0700

    Add SmartHaulers mod skeleton

---
+++ SmartHaulers/Patches/Configurator.cs
@@ -0,0 +1,13 @@
+// Timberborn Mod: SmartHaulers
+// Author: igor.zavoychinskiy@gmail.com
+// License: Public Domain
+
+using Bindito.Core;
+
+namespace IgorZ.SmartHaulers.Patches;
+
+[Context("Game")]
+sealed class Configurator : IConfigurator {
+  public void Configure(IContainerDefinition containerDefinition) {
+  }
+}
